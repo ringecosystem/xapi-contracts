bcf8ad838afdfa7a1a2bca470d3cab26d814c72e
Guantong <i@gt.email>
2024-11-01 11:57:14 +0800

    update consumer

--- xapi-consumer/ignition/modules/ConsumerExample.deploy.js
+++ xapi-consumer/ignition/modules/ConsumerExample.deploy.js
@@ -3,7 +3,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("ConsumerExampleModule", (m) => {
     const deployer = m.getAccount(0);
 
-    const consumer = m.contract("ConsumerExample", ["0x6D83b038d5bF428a8C3fF34bf1b380b3A2acA6fe"], {
+    const consumer = m.contract("ConsumerExample", ["0x258E2B5bFCf2DF7e633B95abDc81A43C2073dA6d"], {
         from: deployer
     });
 
